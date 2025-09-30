ac4424378c3fef067e3d97900c47cc3940f162d4
sujal-GITHUB <sujal21102004@gmail.com>
2025-09-30 09:06:40 +0530

    fixed queues

--- Queues/1.cpp
+++ Queues/1.cpp
@@ -1,61 +1,60 @@
-// Queue using qacks
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Queue{
-    stack<int>s1;
-    stack<int>s2;
+class Queue {
+    stack<int> s1, s2;
 
-    public:
-
-    void push(int x){
+public:
+    void push(int x) {
         s1.push(x);
     }
 
-    void pop(){
-        if(s1.empty())
-        return;
-
-        while(s1.size() > 1){
-            int a = s1.top();
-            s1.pop();
-            s2.push(a);
+    void pop() {
+        if (s1.empty() && s2.empty()) {
+            cout << "Queue is empty\n";
+            return;
         }
 
-        s1.pop();
-
-        swap(s1, s2);
-    };
-
-    int top(){
-        if(s1.empty()) 
-        return -1;
-
-        while(s1.size() > 1){
-            s2.push(s1.top());
-            s1.pop();
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
 
-        int res = s1.top();
-        s1.pop();
+        s2.pop();
+    }
 
-        s2.push(res);
+    int top() {
+        if (s1.empty() && s2.empty()) {
+            cout << "Queue is empty\n";
+            return -1;
+        }
 
-        swap(s1, s2);
-        return res;
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
     }
 };
 
-int main(){
+int main() {
     Queue q;
     q.push(10);
     q.push(20);
     q.push(30);
 
-    cout << q.top() << endl; // 30
+    cout << q.top() << endl; // 10
     q.pop();
     cout << q.top() << endl; // 20
     q.pop();
-    cout << q.top() << endl; // 10
+    cout << q.top() << endl; // 30
 }
